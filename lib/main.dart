dfb39115b41989b1cd217fdca845c3b8b5c96223
mateus-1228 <mateus-1228.ericaeandre44@gmail.com>
2022-06-25 12:42:32 -0300

    Construção da AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,7 +16,8 @@ class MyApp extends StatelessWidget {
       
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Projeto Mercado Livre'),
+       debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -31,44 +32,35 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-    
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
   
     return Scaffold(
       appBar: AppBar(
+       leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.menu,
+                color: Colors.black),
+          ),
+           actions: <Widget>[
+        IconButton(icon: Icon(Icons.shopping_cart_outlined,
+        color: Colors.black,), onPressed: () {}),
+   
+      ],
       
+      backgroundColor: Color(0xfff5d415),
         title: Text(widget.title),
       ),
+     
       body: Center(
         
         child: Column(
-        
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+       
         ),
+        
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+     
+      
     );
   }
 }
